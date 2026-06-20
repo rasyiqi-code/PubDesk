2c1a3fde6b9341b94ab484e83b866724275bac21
Rasyiqi <retaslintasbatas@gmail.com>
2026-06-20 15:31:18 +0700

    style: menggeser koordinat pembatas diagonal miring footer SVG ke kiri sebesar 150 unit

--- src/components/invoice/InvoicePreview.tsx
+++ src/components/invoice/InvoicePreview.tsx
@@ -716,10 +716,10 @@ const InvoicePreview: React.FC<InvoicePreviewProps> = ({ previewProfile, overrid
               {/* Grup shape background horizontal dengan efek timbul (drop shadow) */}
               <g filter="url(#drop-shadow-footer)">
                 {/* Bidang hitam */}
-                <path d="M 0 20 H 462 L 499.5 70 H 0 Z" fill={headerBgColor} />
+                <path d="M 0 20 H 312 L 349.5 70 H 0 Z" fill={headerBgColor} />
 
                 {/* Bidang merah kanan (Warna dinamis) */}
-                <path d="M 538.25 20 H 1045 V 70 H 575.75 Z" fill={headerPrimaryColor} />
+                <path d="M 388.25 20 H 1045 V 70 H 425.75 Z" fill={headerPrimaryColor} />
               </g>
 
               {/* Teks Pengantar Transfer (di dalam shape footer kiri) */}
@@ -756,10 +756,10 @@ const InvoicePreview: React.FC<InvoicePreviewProps> = ({ previewProfile, overrid
               {/* Grup shape miring tengah dengan efek timbul (drop shadow) di atas shape horizontal */}
               <g filter="url(#drop-shadow-middle-footer)">
                 {/* Diagonal merah tengah (Warna dinamis) */}
-                <path d="M 470 5 H 509 L 557.75 70 H 518.75 Z" fill={headerSecondaryColor} />
+                <path d="M 320 5 H 359 L 407.75 70 H 368.75 Z" fill={headerSecondaryColor} />
 
                 {/* Pemisah putih */}
-                <path d="M 509 5 H 527 L 575.75 70 H 557.75 Z" fill="#ffffff" />
+                <path d="M 359 5 H 377 L 425.75 70 H 407.75 Z" fill="#ffffff" />
               </g>
             </svg>
           </div>
